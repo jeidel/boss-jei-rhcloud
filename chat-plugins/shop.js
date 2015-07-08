601d35ed5ced84678715e0d4eff4e604e7ec6fd3
The WleDey <swifer_12@hotmail.com>
2015-07-08 18:37:56 -0430

    Update shop.js

--- chat-plugins/shop.js
+++ chat-plugins/shop.js
@@ -131,7 +131,7 @@ exports.commands = {
 				return this.sendReply("Has solicitado un cambio de tu avatar personalizado. Espera a que un admin revise tu compra.");
 				break;
 			case 'customavatar':
-				prize = 5000;
+				prize = 6000;
 				if (Shop.getUserMoney(user.name) < prize) return this.sendReply("No tienes suficiente dinero.");
 				if (Config.customavatars[user.userid]) return this.sendReply("Ya habías comprado este artículo. Para cambiar tu avatar compra la opcion Avatar");
 				if (params.length !== 2) return this.sendReply("Usa el comando así: /buy avatar,[imagen]");
